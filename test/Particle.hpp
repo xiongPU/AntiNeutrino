7d8c6a2dee73d0ea82ccb5b48c970a30fd34f8f6
xiongPU <junxiongustc@gmail.com>
2015-05-07 11:45:13 -0400

    Particle header file for new data record

---
+++ test/Particle.hpp
@@ -0,0 +1,14 @@
+
+//to include the particle information
+class Particle{
+    Int_t TrackID_;
+};
+
+//use the information from particles to calculate physical quantities
+class Measure{
+public:
+    vector<vector<Particle>> & A;
+}
+
+    
+
